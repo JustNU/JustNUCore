2fe774b2cf9522d5702f1d8d9c655d8c74f9fe0a
JustNU <ivancool108@gmail.com>
2022-12-17 06:50:04 +0300

    Implement item retexture functions

--- src/JustNUCore.ts
+++ src/JustNUCore.ts
@@ -11,7 +11,7 @@ export class JustNUCore {
 	)
 	{}
 	
-	public addTop(modDb: string, outfitID: string, topbundlePath: string, handsbundlePath: string, handsBaseID: string): void
+	public addTop(modDb, outfitID, topbundlePath, handsbundlePath, handsBaseID): void
 	{
 		// const
 		const database = this.databaseServer.getTables();
@@ -19,42 +19,42 @@ export class JustNUCore {
 		const VFS = this.VFS;
 		
 		// add top
-		let newTop = jsonUtil.clone(database.templates.customization["5d28adcb86f77429242fc893"]);
+		const newTop = jsonUtil.clone(database.templates.customization["5d28adcb86f77429242fc893"]);
 
-        newTop._id = outfitID;
-        newTop._name = outfitID;
-        newTop._props.Prefab.path = topbundlePath;
-        database.templates.customization[outfitID] = newTop;
+		newTop._id = outfitID;
+		newTop._name = outfitID;
+		newTop._props.Prefab.path = topbundlePath;
+		database.templates.customization[outfitID] = newTop;
 		
 		// add hands
-		let newHands = jsonUtil.clone(database.templates.customization[handsBaseID]);
+		const newHands = jsonUtil.clone(database.templates.customization[handsBaseID]);
 
-        newHands._id = `${outfitID}Hands`;
-        newHands._name = `${outfitID}Hands`;
-        newHands._props.Prefab.path = handsbundlePath;
-        database.templates.customization[`${outfitID}Hands`] = newHands;
+		newHands._id = `${outfitID}Hands`;
+		newHands._name = `${outfitID}Hands`;
+		newHands._props.Prefab.path = handsbundlePath;
+		database.templates.customization[`${outfitID}Hands`] = newHands;
 		
 		// add suite
-		let newSuite = jsonUtil.clone(database.templates.customization["5d1f623e86f7744bce0ef705"]);
+		const newSuite = jsonUtil.clone(database.templates.customization["5d1f623e86f7744bce0ef705"]);
 
-        newSuite._id = `${outfitID}Suite`;
-        newSuite._name = `${outfitID}Suite`;
+		newSuite._id = `${outfitID}Suite`;
+		newSuite._name = `${outfitID}Suite`;
 		newSuite._props.Body = outfitID;
 		newSuite._props.Hands = `${outfitID}Hands`;
 		newSuite._props.Side = ["Usec", "Bear", "Savage"];
-        database.templates.customization[`${outfitID}Suite`] = newSuite;
+		database.templates.customization[`${outfitID}Suite`] = newSuite;
 		
 		// locale
 		for (const localeID in database.locales.global)
-        {
-            // en placeholder
+		{
+			// en placeholder
 			database.locales.global[localeID].templates[`${outfitID}Suite`] = jsonUtil.deserialize(VFS.readFile(`${modDb}locales/en.json`))[outfitID];
 			
 			// actual locale
 			if (VFS.exists(`${modDb}locales/${localeID}.json`)) {
 				database.locales.global[localeID].templates[`${outfitID}Suite`] = jsonUtil.deserialize(VFS.readFile(`${modDb}locales/${localeID}.json`))[outfitID];
 			}
-        }
+		}
 		
 		// add suite to the ragman
 		database.traders["5ac3b934156ae10c4430e83c"].suits.push({
@@ -78,7 +78,7 @@ export class JustNUCore {
 		});
 	}
 	
-	public addBottom(modDb: string, outfitID: string, bundlePath: string): void
+	public addBottom(modDb, outfitID, bundlePath): void
 	{
 		// const
 		const database = this.databaseServer.getTables();
@@ -86,33 +86,33 @@ export class JustNUCore {
 		const VFS = this.VFS;
 		
 		// add Bottom
-		let newBottom = jsonUtil.clone(database.templates.customization["5d5e7f4986f7746956659f8a"]);
+		const newBottom = jsonUtil.clone(database.templates.customization["5d5e7f4986f7746956659f8a"]);
 
-        newBottom._id = outfitID;
-        newBottom._name = outfitID;
-        newBottom._props.Prefab.path = bundlePath;
-        database.templates.customization[outfitID] = newBottom;
+		newBottom._id = outfitID;
+		newBottom._name = outfitID;
+		newBottom._props.Prefab.path = bundlePath;
+		database.templates.customization[outfitID] = newBottom;
 		
 		// add suite
-		let newSuite = jsonUtil.clone(database.templates.customization["5cd946231388ce000d572fe3"]);
+		const newSuite = jsonUtil.clone(database.templates.customization["5cd946231388ce000d572fe3"]);
 
-        newSuite._id = `${outfitID}Suite`;
-        newSuite._name = `${outfitID}Suite`;
+		newSuite._id = `${outfitID}Suite`;
+		newSuite._name = `${outfitID}Suite`;
 		newSuite._props.Feet = outfitID;
 		newSuite._props.Side = ["Usec", "Bear", "Savage"];
-        database.templates.customization[`${outfitID}Suite`] = newSuite;
+		database.templates.customization[`${outfitID}Suite`] = newSuite;
 		
 		// locale
 		for (const localeID in database.locales.global)
-        {
-            // en placeholder
+		{
+			// en placeholder
 			database.locales.global[localeID].templates[`${outfitID}Suite`] = jsonUtil.deserialize(VFS.readFile(`${modDb}locales/en.json`))[outfitID];
 			
 			// actual locale
 			if (VFS.exists(`${modDb}locales/${localeID}.json`)) {
 				database.locales.global[localeID].templates[`${outfitID}Suite`] = jsonUtil.deserialize(VFS.readFile(`${modDb}locales/${localeID}.json`))[outfitID];
 			}
-        }
+		}
 		
 		// add suite to the ragman
 		database.traders["5ac3b934156ae10c4430e83c"].suits.push({
@@ -135,121 +135,145 @@ export class JustNUCore {
 			}
 		});
 	}
-	
-	/*
-	static AddItemRetexture(db, ItemBase, NewItemID, bundlePath)
-    {
-        // create item
-		let NewItem = jsonUtil.clone(database.templates.items[ItemBase]);
 
-        NewItem._id = NewItemID;
-        NewItem._name = NewItemID;
-        NewItem._props.Prefab.path = bundlePath;
-        database.templates.items[NewItemID] = NewItem;
+	public addItemRetexture(itemId, db, config, itemConfig, itemData): void
+	{
+		// const
+		const database = this.databaseServer.getTables();
+		const jsonUtil = this.jsonUtil;
+		const VFS = this.VFS;
+		const dbItems = database.templates.items;
+		const bundlePath = itemData[itemId].BundlePath;
+		const baseItemID = itemData[itemId].BaseItemID;
 		
-        // handbook
-        let ItemHandbook = jsonUtil.clone(database.templates.handbook.Items.find((item) =>
-        {
-            return item.Id === ItemBase;
-        }));
-
-        ItemHandbook.Id = NewItem._id;
-        database.templates.handbook.Items.push(ItemHandbook);
+		// copy item from DB, change it's values and add it to database
+		const newItem = jsonUtil.clone(dbItems[baseItemID]);
+		newItem._id = itemId;
+		newItem._name = itemId;
+		newItem._props.Prefab.path = bundlePath;
+		dbItems[itemId] = newItem;
+		
+		// copy handbook entry, change its Id, add it to handbook
+		const newItemHandbook = jsonUtil.clone(database.templates.handbook.Items.find((item) => {return item.Id === baseItemID}));
+		newItemHandbook.Id = itemId;
+		database.templates.handbook.Items.push(newItemHandbook);
+		
+		// add it to prices json for flea pricing
+		const newPrice = jsonUtil.clone(database.templates.prices[baseItemID];
+		database.templates.prices[itemId] = newPrice;
 		
 		// locale
 		for (const localeID in database.locales.global)
-        {
+		{
 			// en placeholder
-			database.locales.global[localeID].templates[NewItemID] = jsonUtil.deserialize(VFS.readFile(`${db}locales/en.json`))[NewItemID];
+			database.locales.global[localeID].templates[itemId] = jsonUtil.deserialize(VFS.readFile(`${db}locales/en.json`))[itemId];
 			
 			// actual locale
 			if (VFS.exists(`${db}locales/${localeID}.json`)) {
-				database.locales.global[localeID].templates[NewItemID] = jsonUtil.deserialize(VFS.readFile(`${db}locales/${localeID}.json`))[NewItemID];
+				database.locales.global[localeID].templates[itemId] = jsonUtil.deserialize(VFS.readFile(`${db}locales/${localeID}.json`))[itemId];
 			}
-        }
-		
-		//database.locales.global["en"].templates[NewItemID] = jsonUtil.deserialize(VFS.readFile(`${db}locales/en.json`))[NewItemID];
-		
-		// add item to the ragfair
-        database.traders["ragfair"].assort.items.push({
-            "_id": `${NewItemID}_ragfairOffer`,
-            "_tpl": NewItemID,
-            "parentId": "hideout",
-            "slotId": "hideout",
-            "upd": {
-                "UnlimitedCount": true,
-                "StackObjectsCount": 999999999
-            }
-        });
-		
-		// add purchase cost
-        database.traders["ragfair"].assort.barter_scheme[`${NewItemID}_ragfairOffer`] = [
-            [
-            {
-                "count": NewItem._props.CreditsPrice,
-                "_tpl": "5449016a4bdc2d6f028b456f"
-            }]
-        ]
-		
-		// add trader standing requirement
-        database.traders.ragfair.assort.loyal_level_items[`${NewItemID}_ragfairOffer`] = 1;
+		}
 		
 		// update filters/conflicts
-		let dbItems = database.templates.items;
+		this.updateFilters(itemId, baseItemID);
 		
-		for (const item in dbItems) {
-			const itemConflictId = dbItems[item]._props.ConflictingItems;
+		// trader offers
+		if (config.EnableTradeOffers) {
+			this.copyTradeOffers(itemId, baseItemID)
+		}
+	}
+	
+	public updateFilters(itemId, copyItemID): void
+	{
+		// const
+		const database = this.databaseServer.getTables();
+		
+		for (const item in database.templates.items) {
+			
+			// handle conflicts
+			const itemConflictId = database.templates.items[item]._props.ConflictingItems;
 			
 			for (const itemInConflicts in itemConflictId) {
-				let itemInConflictsFiltersId = itemConflictId[itemInConflicts];
+				const itemInConflictsFiltersId = itemConflictId[itemInConflicts];
 					
-				if (itemInConflictsFiltersId === ItemBase) {
-					itemConflictId.push(NewItemID);
+				if (itemInConflictsFiltersId === copyItemID) {
+					itemConflictId.push(itemId);
 				}
 			}
 			
-			for (const slots in dbItems[item]._props.Slots) {
-				const slotsId = dbItems[item]._props.Slots[slots]._props.filters[0].Filter;
+			// handle filters
+			for (const slots in database.templates.items[item]._props.Slots) {
+				const slotsId = database.templates.items[item]._props.Slots[slots]._props.filters[0].Filter;
 				
 				for (const itemInFilters in slotsId) {
-					let itemInFiltersId = slotsId[itemInFilters]
+					const itemInFiltersId = slotsId[itemInFilters]
 					
-					if (itemInFiltersId === ItemBase) {
-						slotsId.push(NewItemID);
+					if (itemInFiltersId === copyItemID) {
+						slotsId.push(itemId);
 					}
 				}
 			}
 		}
-    }
+	}
+	
+	public copyTradeOffers(itemId, copyItemID): void
+	{
+		// const
+		const database = this.databaseServer.getTables();
+		const jsonUtil = this.jsonUtil;
+		
+		for (const trader in database.traders) {
+			if (trader !== "ragfair") {
+				for (const offer in database.traders[trader].assort.items) {
+					if (database.traders[trader].assort.items[offer]._tpl === copyItemID && database.traders[trader].assort.items[offer].parentId === "hideout" ) {
+						// copy original offer, change its id and tpl, and push to assorts
+						const newOffer = jsonUtil.clone(database.traders[trader].assort.items[offer]);
+						newOffer._id = `${database.traders[trader].assort.items[offer]._id}_${itemId}_${trader}`;
+						newOffer._tpl = itemId;
+						database.traders[trader].assort.items.push(newOffer)
+						
+						// copy price
+						const newPrice = jsonUtil.clone(database.traders[trader].assort.barter_scheme[database.traders[trader].assort.items[offer]._id]);
+						database.traders[trader].assort.barter_scheme[`${database.traders[trader].assort.items[offer]._id}_${itemId}_${trader}`] = newPrice;
+						
+						// copy loyalty level
+						const newLoaylty = jsonUtil.clone(database.traders[trader].assort.loyal_level_items[database.traders[trader].assort.items[offer]._id]);
+						database.traders[trader].assort.loyal_level_items[`${database.traders[trader].assort.items[offer]._id}_${itemId}_${trader}`] = newLoaylty;
+					}
+				}
+			}
+		}
+	}
+	/*
 	
 	static createTraderOffer(ItemID, TraderID, MoneyID, TraderPrice, TraderLvl)
-    {
-        // add item to the trader
-        database.traders[TraderID].assort.items.push(
-        {
-            "_id": `${ItemID}_${TraderID}_${MoneyID}_${TraderPrice}_${TraderLvl}_offer`,
-            "_tpl": ItemID,
-            "parentId": "hideout",
-            "slotId": "hideout",
-            "upd":
-            {
-                "UnlimitedCount": true,
-                "StackObjectsCount": 999999
-            }
-        });
+	{
+		// add item to the trader
+		database.traders[TraderID].assort.items.push(
+		{
+			"_id": `${ItemID}_${TraderID}_${MoneyID}_${TraderPrice}_${TraderLvl}_offer`,
+			"_tpl": ItemID,
+			"parentId": "hideout",
+			"slotId": "hideout",
+			"upd":
+			{
+				"UnlimitedCount": true,
+				"StackObjectsCount": 999999
+			}
+		});
 
-        // add purchase cost
-        database.traders[TraderID].assort.barter_scheme[`${ItemID}_${TraderID}_${MoneyID}_${TraderPrice}_${TraderLvl}_offer`] = [
-            [
-            {
-                "count": TraderPrice,
-                "_tpl": MoneyID
-            }]
-        ]
+		// add purchase cost
+		database.traders[TraderID].assort.barter_scheme[`${ItemID}_${TraderID}_${MoneyID}_${TraderPrice}_${TraderLvl}_offer`] = [
+			[
+			{
+				"count": TraderPrice,
+				"_tpl": MoneyID
+			}]
+		]
 
-        // add trader loyalty requirement
-        database.traders[TraderID].assort.loyal_level_items[`${ItemID}_${TraderID}_${MoneyID}_${TraderPrice}_${TraderLvl}_offer`] = TraderLvl;
-    }
+		// add trader loyalty requirement
+		database.traders[TraderID].assort.loyal_level_items[`${ItemID}_${TraderID}_${MoneyID}_${TraderPrice}_${TraderLvl}_offer`] = TraderLvl;
+	}
 	
 	static createBotSpawnPoint(spawnPointName, xCoordinate, yCoordinate, zCoordinate, map, botZoneName, RequiresCordFix = true) 
 	{
@@ -333,28 +357,6 @@ export class JustNUCore {
 		
 		database.locations[map].base.SpawnPointParams.push(spawnPoint)
 	}
-	
-	static CopyTradeOffers(originalId, newItemId)
-    {
-		for (const trader in database.traders) {
-			if (trader !== "ragfair") {
-				for (const offer in database.traders[trader].assort.items) {
-					if (database.traders[trader].assort.items[offer]._tpl === originalId && database.traders[trader].assort.items[offer].parentId === "hideout" ) {
-						let newOffer = jsonUtil.clone(database.traders[trader].assort.items[offer]);
-						newOffer._id = `${database.traders[trader].assort.items[offer]._id}_${newItemId}_${trader}`;
-						newOffer._tpl = newItemId;
-						database.traders[trader].assort.items.push(newOffer)
-						
-						let newPrice = jsonUtil.clone(database.traders[trader].assort.barter_scheme[database.traders[trader].assort.items[offer]._id]);
-						database.traders[trader].assort.barter_scheme[`${database.traders[trader].assort.items[offer]._id}_${newItemId}_${trader}`] = newPrice;
-						
-						let newLoaylty = jsonUtil.clone(database.traders[trader].assort.loyal_level_items[database.traders[trader].assort.items[offer]._id]);
-						database.traders[trader].assort.loyal_level_items[`${database.traders[trader].assort.items[offer]._id}_${newItemId}_${trader}`] = newLoaylty;
-					}
-				}
-			}
-		}
-    }
 	
 	*/
 }
